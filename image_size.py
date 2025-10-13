0e5bd2a20b085001b7823859bf541e4d7537d927
helloworldz0 <aditya.sharma.hyd@gmail.com>
2025-10-13 11:45:39 +0530

    v3.4

--- image_size.py
+++ image_size.py
@@ -1,9 +1,9 @@
 import PIL
 from PIL import Image
 
-folder_path='./Data/Black_White/' 
-img='gray_8n.jpg'
-img=folder_path+img
+folder_path = './Data/Grayscale_2/' 
+img = '1664.jpg'
+img = folder_path + img
 
 width, height = PIL.Image.open(img).size
 print(width, height)
